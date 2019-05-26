f6bac84bbdd2e418c1bc1071ade890a287fd4ae9
Georgy Marchuk <gmarcuk@gmail.com>
2019-05-26 20:04:23 +0200

    update swup + add debug plugin + add js plugin

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
-import Swupjs from 'swupjs';
+import Swup from 'swup';
+import JsPlugin from '@swup/js-plugin';
+import DebugPlugin from '@swup/debug-plugin';
 import { TweenMax } from 'gsap/TweenMax';
 import loadComponents from 'gia/loadComponents';
 import components from './components';
@@ -7,27 +9,31 @@ import components from './components';
 loadComponents(components);
 
 // enable swup
-const swup = new Swupjs({
-    debugMode: true,
-    LINK_SELECTOR: 'a[href^="' + window.location.origin + '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
-    animations: {
-        '*': {
-            in: function(next){
-                document.querySelector('#swup').style.opacity = 0;
-                TweenMax.to(document.querySelector('#swup'), .5, {
-                    opacity: 1,
-                    onComplete: next
-                });
+const swup = new Swup({
+    linkSelector: 'a[href^="' + window.location.origin + '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
+    plugins: [
+        new JsPlugin([
+            {
+                from: '(.*)',
+                to: '(.*)',
+                in: (next) => {
+                    document.querySelector('#swup').style.opacity = 0;
+                    TweenMax.to(document.querySelector('#swup'), .5, {
+                        opacity: 1,
+                        onComplete: next
+                    });
+                },
+                out: (next) => {
+                    document.querySelector('#swup').style.opacity = 1;
+                    TweenMax.to(document.querySelector('#swup'), .5, {
+                        opacity: 0,
+                        onComplete: next
+                    });
+                }
             },
-            out: function(next){
-                document.querySelector('#swup').style.opacity = 1;
-                TweenMax.to(document.querySelector('#swup'), .5, {
-                    opacity: 0,
-                    onComplete: next
-                });
-            }
-        },
-    }
+        ]),
+        new DebugPlugin()
+    ]
 });
 
 // reload components for each container after transition
